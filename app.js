ff44e0de25550998ed3bb277e31c1438a82cb0f4
gustavotonolli <gustavo.tonolli@hotmail.com>
2023-09-08 22:35:45 -0300

    login implemented

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+var btnSignin = document.querySelector("#signin");
+var btnSignup = document.querySelector("#signup");
 
+// var btnLogin = document.querySelector("#login");
+
+var body = document.querySelector("body");
+
+btnSignin.addEventListener("click", function() {
+    body.className = "sign-in-js";
+})
+
+btnSignup.addEventListener("click", function() {
+    body.className = "sign-up-js";
+})
+function getLogin() {
+    window.location.href = "./index.html"
+    console.log("oi")
+}
