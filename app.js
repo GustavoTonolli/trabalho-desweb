d261dc3b66605f4d24835f02ccf58257c5d5b1f0
gustavotonolli <gustavo.tonolli@hotmail.com>
2023-09-09 21:58:02 -0300

    added services

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var btnSignin = document.querySelector("#signin");
 var btnSignup = document.querySelector("#signup");
 
@@ -12,3 +13,4 @@ btnSignin.addEventListener("click", function() {
 btnSignup.addEventListener("click", function() {
     body.className = "sign-up-js";
 })
+
